66e642a4be1e1e23799a71c61b802610e04aa82c
Ihar Trymailau <ihar.trymailau@gmail.com>
2019-07-23 16:31:24 +0300

    Update workspace.xml

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './../App.css';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src='https://styleguide.audiomack.com/styleguide/assets/images/logos/mark-orange.png'/>
         </header>
     );
