38a56c5210a0f181a81dc54bcbabf513411ea179
Ihar Trymailau <ihar.trymailau@gmail.com>
2019-08-01 13:12:03 +0300

    upgrade dialogpage + props + navlink

--- src/components/DialogsPage/DialogsPage.js
+++ src/components/DialogsPage/DialogsPage.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import s from './DialogsPage.module.css';
+import {NavLink} from "react-router-dom";
+
+const Dialog = (props) => {
+    return (
+        <div className={s.dialog}>
+            <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+        </div>
+    )
+}
+const Messege = (props) => {
+    return <div className={s.messege}>{props.messege}</div>
+}
 
 const DialogsPage = () => {
     return (
@@ -7,19 +19,18 @@ const DialogsPage = () => {
             <div className={s.dialogs}>
                 <h1>DIALOGS</h1>
             </div>
-            <div className={s.messegeListLeft}>
-                <ul>
-                    <li>Andrey</li>
-                    <li>Dmitry</li>
-                    <li>Sasha</li>
-                    <li>Sveta</li>
-                    <li>Ihar</li>
-                    <li>Julia</li>
-                    <li>Alexey</li>
-                </ul>
+            <div className={s.dialogsListLeft}>
+                <Dialog name='Ihar' id='1'/>
+                <Dialog name='Julia' id='2'/>
+                <Dialog name='Dmitry' id='3'/>
+                <Dialog name='Alexey' id='4'/>
+                <Dialog name='Sasha' id='5'/>
+                <Dialog name='Artem' id='6'/>
             </div>
             <div className={s.messegeRight}>
-                <div className={s.dialogMessege}>messege user</div>
+                <Messege messege='Messege user'/>
+                <Messege messege='Messege user'/>
+                <Messege messege='Messege user'/>
             </div>
         </div>
     );
