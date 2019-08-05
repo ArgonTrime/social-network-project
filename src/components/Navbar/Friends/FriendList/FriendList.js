92be7efd61082fb725346cfb634a98fbe707f2c5
Ihar Trymailau <ihar.trymailau@gmail.com>
2019-08-05 21:06:04 +0300

    fix bag friendLIst
    friends list element one row + fix backgroundcolor

--- src/components/Navbar/Friends/FriendList/FriendList.js
+++ src/components/Navbar/Friends/FriendList/FriendList.js
@@ -5,7 +5,7 @@ import s from './FriendList.module.css';
 const FriendsList = (props) => {
     return (
 
-            <div>
+            <div className={s.friendList}>
                 <img src='https://www.shareicon.net/download/2017/02/05/878221_user_512x512.png'/>
                 <div>{props.name}</div>
 
