23e4a415f61a0376031abce7b76c45357ae255f9
Ihar Trymailau <ihar.trymailau@gmail.com>
2019-09-25 13:56:49 +0300

    fix side effect users, add showUsers button

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,38 +4,43 @@ import * as axios from 'axios';
 import userAvatar from '../../image/user.png';
 
 let Users = (props) => {
+    let showUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items);
+            })
 
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items);
-        })
-
-        // props.setUsers([
-        //     {
-        //         id: 1,
-        //         fullname: 'Ihar',
-        //         status:'Learning web-development',
-        //         location: {country: 'Belarus', city: 'Minsk'},
-        //         followCheck: false,
-        //         avatarUrl: 'https://cdn0.iconfinder.com/data/icons/user-pictures/100/matureman1-512.png'
-        //     },
-        //     {
-        //         id: 2,
-        //         fullname: 'Julia',
-        //         status:'Looking for the best',
-        //         location: {country: 'Belarus', city: 'Minsk'},
-        //         followCheck: true,
-        //         avatarUrl: 'https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png'
-        //     }
-        // ])
+            // props.setUsers([
+            //     {
+            //         id: 1,
+            //         fullname: 'Ihar',
+            //         status:'Learning web-development',
+            //         location: {country: 'Belarus', city: 'Minsk'},
+            //         followCheck: false,
+            //         avatarUrl: 'https://cdn0.iconfinder.com/data/icons/user-pictures/100/matureman1-512.png'
+            //     },
+            //     {
+            //         id: 2,
+            //         fullname: 'Julia',
+            //         status:'Looking for the best',
+            //         location: {country: 'Belarus', city: 'Minsk'},
+            //         followCheck: true,
+            //         avatarUrl: 'https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png'
+            //     }
+            // ])
+        }
     }
-
     return <div>
+        <button onClick={showUsers} className={s.btnF}>Show users</button>
         {
             props.users.map(u => <div className={s.container} key={u.id}>
                 <div className={s.box1}>
                     <div><img src={u.photos.small != null ? u.photos.small : userAvatar}/></div>
-                    <div>{u.followed ? <button className={s.btnU} onClick={() => {props.unfollow(u.id)}}>Unfollow</button> : <button className={s.btnF} onClick={() => {props.follow(u.id)}}>Follow</button>}</div>
+                    <div>{u.followed ? <button className={s.btnU} onClick={() => {
+                        props.unfollow(u.id)
+                    }}>Unfollow</button> : <button className={s.btnF} onClick={() => {
+                        props.follow(u.id)
+                    }}>Follow</button>}</div>
                 </div>
 
                 <div className={s.box2}>
